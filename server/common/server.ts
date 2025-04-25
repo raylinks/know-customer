24c69b3f3c8b61652e6ea3973a36a66698f08bd3
olawuyi rilwan <olawuyirilwan98@gmail.com>
2025-04-25 21:54:41 +0100

    comment

--- server/common/server.ts
+++ server/common/server.ts
@@ -15,6 +15,7 @@ const env = JSON.parse(process.env.APP_SECRETS ?? '{}');
 
 // app.use(bodyParser.json())
 
+//commented
 // unleash.on('synchronized', () => {
 //   l.info('Feature flags loaded');
 // });
